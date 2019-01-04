2689cd548201566658908f83aae8c0dedd964a19
Bernardo <bernaferrari2@gmail.com>
2019-01-04 15:07:00 -0200

    Init project.

---
+++ README.md
@@ -0,0 +1,124 @@
+Gradle Kotlin DSL converter
+=================
+
+[![License](https://img.shields.io/badge/license-Apache%20License%202.0-blue.svg?style=flat)](http://www.apache.org/licenses/LICENSE-2.0)
+
+Welcome! After a lot of pain trying to migrate from Gradle's Groovy to Kotlin DSL on Android Studio, I developed this tool to solve most migration issues and reduce the amount of work you need in order to make things ready.
+
+Please see this tool as a helper, a first resource like Android Studio's Java converter to Kotlin: it won't make a _perfect_ conversion, but it helps a lot in reducing the time you would spend on repetitive tasks.
+
+On my own apps, I've used apostrophes \' instead of quotation marks \" since forever, so it wasn't fun when I discovered I would need to modify more than 100 lines of code to make Kotlin DSL work. Besides this, the tool also solves a few common issues that might appear, like the ```task clean(type: Delete)``` that becomes a completely different thing.
+
+
+Getting Started
+---------------
+
+The tool was developed using [kscript](https://github.com/holgerbrandl/kscript). You can find how to install it [here](https://github.com/holgerbrandl/kscript#installation). 
+Gradle 5 is [currently incompatible](https://github.com/holgerbrandl/kscript/issues/197) with the tool that packages it, so I'm afraid you will need to [install kscript](https://github.com/holgerbrandl/kscript#installation) before running this tool. It should be release easy, however. After that, you can run the script by calling:
+
+```
+./gradlekotlinconverter.kts build.gradle
+```
+or 
+```
+kscript gradlekotlinconverter.kts build.gradle
+```
+
+Things it can do
+---------------
+
+<table>
+    <th>Description</th>
+    <th>Before</th>
+    <th>After</th>
+    <tr>
+        <td>Replace all ' with "</td>
+        <td>'kotlin-android'</td>
+        <td>"kotlin-android"</td>
+    </tr>
+    <tr>
+        <td>Replace "def " with "val "</td>
+        <td>def appcompat = "1.0.0"</td>
+        <td>val appcompat = "1.0.0"</td>
+    </tr>
+    <tr>
+        <td>Convert plugins to Kotlin DSL</td>
+        <td>apply plugin: "kotlin-kapt"</td>
+        <td>apply(plugin = "kotlin-kapt")</td>
+    </tr>
+    <tr>
+        <td>Add ( ) to dependencies</td>
+        <td>implementation ":epoxy"</td>
+        <td>implementation(":epoxy")>td>
+    </tr>
+    <tr>
+        <td>Convert Maven to Kotlin DSL</td>
+        <td>maven { url "https://jitpack.io" }</td>
+        <td>maven("https://jitpack.io")</td>
+    </tr>
+    <tr>
+        <td>Convert Sdk Version</td>
+        <td>compileSdkVersion 28</td>
+        <td>compileSdkVersion(28)</td>
+    </tr>
+    <tr>
+        <td>Convert Version Code</td>
+        <td>versionCode 4</td>
+        <td>versionCode = 4</td>
+    </tr>
+    <tr>
+        <td>Convert Build Types</td>
+        <td>debuggable true</td>
+        <td>isDebuggable = true</td>
+    </tr>
+    <tr>
+        <td>Convert proguardFiles</td>
+        <td>proguardFiles getDef..., "..."</td>
+        <td>setProguardFiles(listOf(getDef..., "...")</td>
+    </tr>
+    <tr>
+        <td>Convert sourceCompatibility</td>
+        <td>sourceCompatibility = "1.8"</td>
+        <td>sourceCompatibility = JavaVersion.VERSION_1_8</td>
+    </tr>
+     <tr>
+         <td>Convert AndroidExtensions</td>
+         <td>androidExtensions { experimental = true }</td>
+         <td>androidExtensions { isExperimental = true }</td>
+     </tr>
+</table>
+
+You can find all the details on the source code.
+
+Things it still can't do
+-------
+- Dealing with _testInstrumentationRunner_
+- To avoid bugs, all plugins should be in one block ([3th law of SUPERCILEX](https://twitter.com/SUPERCILEX/status/1079832024456749059))
+- On settings.gradle, include ":app", ":diffutils" should become include (":app", ":diffutils")
+- Dealing with signingConfigs and buildTypes. On singingConfigs, _release_ should become _register("release")_. But there can be multiple arguments, like debug. On buildTypes, _release_ becomes _named("release")_.
+- If you find anything else, just tell me.
+
+
+Issue Tracking
+-------
+Found a bug? Have an idea for an improvement? Feel free to [add an issue](../../issues).
+
+License
+-------
+
+Copyright 2018 Bernardo Ferrari.
+
+Licensed to the Apache Software Foundation (ASF) under one or more contributor
+license agreements.  See the NOTICE file distributed with this work for
+additional information regarding copyright ownership.  The ASF licenses this
+file to you under the Apache License, Version 2.0 (the "License"); you may not
+use this file except in compliance with the License.  You may obtain a copy of
+the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+License for the specific language governing permissions and limitations under
+the License.
---
+++ gradlekotlinconverter.kts
@@ -0,0 +1,282 @@
+#!/usr/bin/env kscript
+
+import java.io.File
+import kotlin.system.exitProcess
+
+// Bernardo Ferrari
+// APACHE-2 License
+
+val intro = """
+Welcome to Gradle Kotlin DSL converter!
+
+This is a helper tool, much like Android Studio's Java -> Kotlin converter.
+It is not perfect and there will be things that you'll need to manually solve, but this helps reducing the amount of work.
+It takes care of adding ( ) to dependencies, converting ' to ", adapting plugins and a lot else.
+
+Usage:
+    GradleKotlinConverter.kts <build.gradle file>
+    kscript GradleKotlinConverter.kts <build.gradle file>
+"""
+
+println(intro)
+
+val input = if (args.isEmpty()) {
+    println("I see you din't select a build.gradle file to convert. Please type the file path:")
+    readLine()
+} else {
+    args.first()
+}
+
+println("Trying to open file...")
+val file = File(input)
+if (!file.exists()) {
+    println("Didn't find a file in the path you specified. Exiting...")
+    exitProcess(0)
+}
+println("File opened successfully...")
+
+
+// anything with ' ('1.0.0', 'kotlin-android', 'jitpack', etc)
+// becomes
+// anything with " ("1.0.0", "kotlin-android", "jitpack", etc)
+fun String.replaceApostrophes(): String = this.replace("'", "\"")
+
+
+// def appcompat = "1.0.0"
+// becomes
+// val appcompat = "1.0.0"
+fun String.replaceDefWithVal(): String = this.replace("def ", "val ")
+
+
+// apply plugin: "kotlin-android"
+// becomes
+// apply(plugin = "kotlin-android")
+fun String.convertPlugins(): String {
+    val pluginsExp = "apply plugin:((\\s*\"\\S*\")|(\\s*[(]\"\\S*\"[)]))".toRegex()
+
+    return this.replace(pluginsExp) {
+        val pluginIdExp = "\"\\S*\"".toRegex()
+        // it identifies the plugin id and rebuilds the line.
+        "apply(plugin = ${pluginIdExp.find(it.value)?.value})"
+    }
+}
+
+
+// implementation ":epoxy-annotations"
+// becomes
+// implementation(":epoxy-annotations")
+fun String.convertDependencies(): String {
+
+    val dependencies = "dependencies\\s*\\{[\\s\\S]*}".toRegex()
+
+    val gradleKeywords = "(implementation|testImplementation|api|annotationProcessor|classpath|kapt)"
+
+    val identifyWord = "($gradleKeywords.*)".toRegex()
+
+    return dependencies.find(this)?.value?.replace(identifyWord) {
+
+        // the function matched something but don't know which keyword was matched
+        // this will be necessary to reconstruct  it
+        val getGradleKeyword = gradleKeywords.toRegex().find(it.value)?.value
+
+        // split: implementation ':epoxy-annotations' becomes 'epoxy-annotations'
+        val splitted = it.value.split(gradleKeywords.toRegex(), 2)[1].let {
+            if (it.startsWith(" ")) it.substring(1) else it
+        }
+
+        // can't be && for the kapt project(':epoxy-processor') scenario
+        if (splitted.first() != '(' || splitted.last { it != ' ' } != ')') {
+            "$getGradleKeyword($splitted)"
+        } else {
+            "$getGradleKeyword$splitted"
+        }
+    } ?: this
+}
+
+
+// NOT WORKING
+//// buildTypes { \n release { ... } }
+//// becomes
+//// buildTypes { \n register("release") { ... } }
+//fun String.convertBuildTypes(): String {
+//
+//    val dependencies = "(?:(buildTypes\\s*\\{)*)\\{(?:[^{]*)(?!}[^{]*})".toRegex()
+//    val rep = "[{ \\s]*".toRegex()
+//    return dependencies.find(this)?.value?.replace(rep) { "named(\"${it.value}\")" } ?: this
+//}
+
+// NOT WORKING
+// buildTypes { \n release { ... } }
+// becomes
+// buildTypes { \n register("release") { ... } }
+//fun String.convertSigningConfigs(): String {
+//
+//    val dependencies = "(?:(signingConfigs\\s*\\{)*)\\{(?:[^{]*)(?!}[^{]*})".toRegex()
+//    val rep = "[{ \\s]*".toRegex()
+//    return dependencies.find(this)?.value?.replace(rep) { "register(\"${it.value}\")" } ?: this
+//}
+
+
+// maven { url "https://maven.fabric.io/public" }
+// becomes
+// maven("https://maven.fabric.io/public")
+fun String.convertMaven(): String {
+
+    val mavenExp = "maven\\s*\\{\\s*url\\s*.*\\s*}\\s*".toRegex()
+
+    return this.replace(mavenExp) {
+        it.value.replace("(url)|( )".toRegex(), "")
+                .replace("{", "(")
+                .replace("}", ")")
+    }
+}
+
+
+// compileSdkVersion 28
+// becomes
+// compileSdkVersion(28)
+fun String.convertSdkVersion(): String {
+
+    val sdkExp = "(compileSdkVersion|minSdkVersion|targetSdkVersion)\\s*\\d*".toRegex()
+
+    return this.replace(sdkExp) {
+        val split = it.value.split(" ")
+
+        // if there is more than one whitespace, the last().toIntOrNull() will find.
+        if (split.lastOrNull { it.toIntOrNull() != null } != null) {
+            "${split[0]}(${split.last()})"
+        } else {
+            it.value
+        }
+    }
+}
+
+
+// versionCode 4
+// becomes
+// versionCode = 4
+fun String.convertVersionCode(): String {
+
+    val versionExp = "(applicationId|versionCode|versionName)\\s*\\S*".toRegex()
+
+    return this.replace(versionExp) {
+        val split = it.value.split(" ")
+
+        // if there is more than one whitespace, the last().toIntOrNull() will find.
+        if (split.lastOrNull { it.isNotBlank() } != null) {
+            "${split[0]} = ${split.last()}"
+        } else {
+            it.value
+        }
+    }
+}
+
+
+// debuggable true
+// becomes
+// isDebuggable = true
+fun String.convertBuildTypesInternal(): String {
+
+    val typesExp = "(debuggable|minifyEnabled|shrinkResources|abortOnError)\\s*\\S*".toRegex()
+
+    return this.replace(typesExp) {
+        val split = it.value.split(" ")
+
+        // if there is more than one whitespace, the last().toIntOrNull() will find.
+        if (split.lastOrNull { it.isNotBlank() } != null) {
+            "is${split[0].capitalize()} = ${split.last()}"
+        } else {
+            it.value
+        }
+    }
+}
+
+
+// proguardFiles getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+// becomes
+// setProguardFiles(listOf(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+fun String.convertProguardFiles(): String {
+
+    val proguardExp = "proguardFiles .*".toRegex()
+
+    return this.replace(proguardExp) {
+        val isolatedArgs = it.value.replace("proguardFiles\\s*".toRegex(), "")
+        "setProguardFiles(listOf($isolatedArgs)"
+    }
+}
+
+
+// sourceCompatibility = "1.8"
+// becomes
+// sourceCompatibility = JavaVersion.VERSION_1_8
+fun String.convertJavaCompatibility(): String {
+
+    val compatibilityExp = "(sourceCompatibility|targetCompatibility)\\s*=\\s*\".*\"\\s*".toRegex()
+
+    return this.replace(compatibilityExp) {
+        val split = it.value.replace("[ \"]*".toRegex(), "").split("=")
+
+        if (split.lastOrNull() != null) {
+            "${split[0]} = JavaVersion.VERSION_${split.last().replace(".", "_")}"
+        } else {
+            it.value
+        }
+    }
+}
+
+
+// converts the clean task, which is very common to find
+fun String.convertCleanTask(): String {
+
+    val cleanExp = "task clean\\(type: Delete\\)\\s*\\{[\\s\\S]*}".toRegex()
+    val registerClean = "tasks.register<Delete>(\"clean\").configure {\n" +
+            "    delete(rootProject.buildDir)\n }"
+
+    return this.replace(cleanExp, registerClean)
+}
+
+
+// androidExtensions { experimental = true }
+// becomes
+// androidExtensions { isExperimental = true }
+fun String.convertAndroidExtensions(): String {
+
+    val extensionsExp = "androidExtensions\\s*\\{[\\s\\S]*}".toRegex()
+
+    return this.replace(extensionsExp) {
+        it.value.replace("experimental", "isExperimental")
+    }
+}
+
+
+// TODO
+// testInstrumentationRunner becomes what?
+// plugin should become one block (3th law of SUPERCILEX: https://twitter.com/SUPERCILEX/status/1079832024456749059)
+// settings.gradle | include ":app", ":diffutils" => include (":app", ":diffutils")
+// signingConfigs and buildTypes
+// do you have any other needs? Please open an issue.
+
+
+val text = file.readText()
+        .replaceApostrophes()
+        .replaceDefWithVal()
+        .convertPlugins()
+        .convertDependencies()
+        .convertMaven()
+        .convertSdkVersion()
+        .convertVersionCode()
+        .convertJavaCompatibility()
+        .convertCleanTask()
+        .convertBuildTypesInternal()
+        .convertProguardFiles()
+        .convertAndroidExtensions()
+
+val newFilePath = file.path + ".kts"
+println("Conversion successful. Saving to: $newFilePath")
+
+val newFile = File(newFilePath)
+newFile.createNewFile()
+newFile.writeText(text)
+
+println("Done!! Thanks for using my script! Exiting...")
+exitProcess(0)
