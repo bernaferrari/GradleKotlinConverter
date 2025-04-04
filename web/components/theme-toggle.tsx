557ac1ef7e65a65eaa9f7f36f2e4c78a8af63130
Bernardo Ferrari <bernaferrari2@gmail.com>
2025-04-04 01:58:26 -0300

    Fix editor flashing

--- web/components/theme-toggle.tsx
+++ web/components/theme-toggle.tsx
@@ -7,13 +7,13 @@ import { useTheme } from "next-themes"
 import { Button } from "@/components/ui/button"
 
 export function ThemeToggle() {
-  const { setTheme, theme } = useTheme()
+  const { setTheme, resolvedTheme } = useTheme()
 
   return (
     <Button
       variant="ghost"
       size="icon"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      onClick={() => setTheme(resolvedTheme === "light" ? "dark" : "light")}
     >
       <Sun className="h-[1.5rem] w-[1.3rem] dark:hidden" />
       <Moon className="hidden h-5 w-5 dark:block" />
