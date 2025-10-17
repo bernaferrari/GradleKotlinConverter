72dd54a0114778b183bb34349c7297d0f73a54ba
Bernardo Ferrari <bernaferrari2@gmail.com>
2025-10-16 22:13:08 -0300

    fix example

--- web/app/default-example.ts
+++ web/app/default-example.ts
@@ -1,163 +1,180 @@
-export const defaultExample = `import org.jetbrains.kotlin.config.KotlinCompilerVersion
-
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-}
-apply(plugin = ("kotlin-kapt"))
-apply(plugin = ("kotlin-android-extensions"))
+export const defaultExample = `apply plugin: 'com.android.application'
+apply plugin: "kotlin-android"
+apply plugin: ('kotlin-kapt')
+apply plugin: ("kotlin-android-extensions")
 
 dependencies {
-    classpath("com.android.tools.build:gradle:3.4.0-alpha10")
-    classpath(kotlin("gradle-plugin", version = "$KOTLIN_VERSION"))
-    classpath("com.bmuschko:gradle-nexus-plugin:2.3.1")
+    classpath 'com.android.tools.build:gradle:3.4.0-alpha10'
+    classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$KOTLIN_VERSION"
+    classpath ('com.bmuschko:gradle-nexus-plugin:2.3.1')
     classpath("com.github.ben-manes:gradle-versions-plugin:0.20.0")
 }
 
 // this should not be converted to block, like the first ones
-apply(plugin = ("gms"))
+apply plugin: ("gms")
 
 plugins {
-    id("io.gitlab.arturbosch.detekt") version "1.0.0.RC8"
-    id("io.gitlab.arturbosch.detekt")
+    id "io.gitlab.arturbosch.detekt" version "1.0.0.RC8"
+    id "io.gitlab.arturbosch.detekt"
 }
 
 dependencies {
   ext {
-    plugins {
-      id("com.android.application")
-      id("kotlin-android")
-    }
+     def test = '1.0.1'
   }
-}
-apply(plugin = ("kotlin-kapt"))
-apply(plugin = ("kotlin-android-extensions"))
-
-dependencies {
-    classpath("com.android.tools.build:gradle:3.4.0-alpha10")
-    classpath(kotlin("gradle-plugin", version = "$KOTLIN_VERSION"))
-    classpath("com.bmuschko:gradle-nexus-plugin:2.3.1")
-    classpath("com.github.ben-manes:gradle-versions-plugin:0.20.0")
-}
-
-// this should not be converted to block, like the first ones
-apply(plugin = ("gms"))
-
-plugins {
-    id("io.gitlab.arturbosch.detekt") version "1.0.0.RC8"
-    id("io.gitlab.arturbosch.detekt")
-}
-
-dependencies {
-  ext {
-     val test = "1.0.1"
-  }
-  val highdef = "1.1.0"
+  def highdef = "1.1.0"
   val testing = "1.0.2"
-  implementation(rootProject.deps.androidPagingComponent)
-  implementation(project(":customer-api")) // comment!
-  kapt(project(":epoxy-processor"))
-  implementation(":epoxy-annotations")
-  val ANDROIDX_APPCOMPAT = "1.0.0"
-  implementation("androidx.appcompat:appcompat:$ANDROIDX_APPCOMPAT")
-  testImplementation("com.randomDependency:1.0.0")
-  compileOnly(group = "commons-io", name = "commons-io", version = "2.6")
-  testCompileOnly(group = "org.apache.commons", name = "commons-math3", version = "3.6.1")
+  implementation rootProject.deps.androidPagingComponent
+  implementation project(':customer-api') // comment!
+  kapt project(':epoxy-processor')
+  implementation ':epoxy-annotations'
+  def ANDROIDX_APPCOMPAT = '1.0.0'
+  compile "androidx.appcompat:appcompat:$ANDROIDX_APPCOMPAT"
+  testCompile "com.randomDependency:1.0.0"
+  compileOnly group: 'commons-io', name: 'commons-io', version: '2.6'
+  testCompileOnly group: 'org.apache.commons', name: 'commons-math3', version: '3.6.1'
+  customFlavorImplementation 'a.lib.com'
+  coreLibraryDesugaring 'com.android.tools:desugar_jdk_libs:1.2.2'
+  detektPlugins 'io.gitlab.arturbosch.detekt:detekt-formatting:1.21.0'
+  ksp 'com.squareup.moshi:moshi-kotlin-codegen:1.14.0'
+  implementation "androidx.compose.ui:ui:1.3.2"
+  lintPublish project(':my-lints')
+  lintCheck project(':my-lints')
 }
 
-testImplementation(group = "junit", name = "junit", version = "4.12")
+testImplementation(group: "junit", name: "junit", version: "4.12")
 
-configurations.classpath {
-    exclude(group = "com.android.tools.external.lombok")
-}
+configurations.classpath.exclude group: 'com.android.tools.external.lombok'
 
-implementation(kotlin("stdlib", KotlinCompilerVersion.VERSION))
-implementation(kotlin("stdlib", KotlinCompilerVersion.VERSION))
+implementation "org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version"
+implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version"
+releaseImplementation "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version"
+debugImplementation "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version"
 
-classpath(kotlin("gradle-plugin", version = "$kotlin_version"))
-classpath(kotlin("gradle-plugin", version = "1.3.20"))
+classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.20")
 
-implementation(kotlin("reflect"))
-testImplementation(kotlin("test"))
-testImplementation(kotlin("test-junit"))
+implementation "org.jetbrains.kotlin:kotlin-reflect"
+testImplementation "org.jetbrains.kotlin:kotlin-test"
+testImplementation "org.jetbrains.kotlin:kotlin-test-junit"
 
-include(":app", ":diffutils")
+include ":app", ':diffutils'
 
-include(
-":common",
-        ":server",
-        ":app", ":core", ":core-model", ":core-data", ":core-ui",
-        ":shared", ":shared-scouting",
-        ":feature-teams", ":feature-autoscout",
-        ":feature-scouts", ":feature-templates", ":feature-settings",
-        ":feature-exports"
-)
+include ':common',
+        ':server',
+        ':app', ':core', ':core-model', ':core-data', ':core-ui',
+        ':shared', ':shared-scouting',
+        ':feature-teams', ':feature-autoscout',
+        ':feature-scouts', ':feature-templates', ':feature-settings',
+        ':feature-exports'
 
 androidExtensions {
-    isExperimental = true
+    experimental = true
 }
 
 allprojects {
     repositories {
         google()
         jcenter()
-        maven("https://jitpack.io")
-        maven("https://maven.fabric.io/public")
-        maven("https://dl.bintray.com/kotlin/kotlin-eap")
+        maven { url = uri("https://jitpack.io") }
+        maven { url 'https://maven.fabric.io/public' }
+        maven { url 'https://dl.bintray.com/kotlin/kotlin-eap' }
     }
 }
 
-compileSdkVersion(28)
+compileSdkVersion 28
 
 defaultConfig {
-    applicationId = "com.bernaferrari.sdkmonitor"
-    minSdkVersion(21)
-    targetSdkVersion(28)
-    versionCode = 4
-    versionName = "0.4"
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    applicationId "com.bernaferrari.sdkmonitor"
+    minSdkVersion 21
+    targetSdkVersion 28
+    versionCode 4
+    versionName "0.4"
+    testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
+    consumerProguardFiles 'consumer-rules.pro'
 }
 buildTypes {
-    named("release"){
-        isDebuggable = false
-        isMinifyEnabled = true
-        isShrinkResources = true
-        setProguardFiles(listOf(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"))
-        signingConfig = signingConfigs.getByName("release")
+    release {
+        multiDexEnabled true
+        debuggable false
+        minifyEnabled true
+        shrinkResources true
+        proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        signingConfig signingConfigs.release
     }
-    named("debug"){
-        isDebuggable = true
-        extra["alwaysUpdateBuildId"] = false
-        extra["enableCrashlytics"] = false
+    debug {
+        debuggable true
+        ext.alwaysUpdateBuildId = false
+        ext.enableCrashlytics = false
     }
 }
 signingConfigs {
-    register("release"){
-        storeFile = file(RELEASE_STORE_FILE)
-        storePassword = RELEASE_STORE_PASSWORD
-        keyAlias = RELEASE_KEY_ALIAS
-        keyPassword = RELEASE_KEY_PASSWORD
+    release {
+        storeFile file(RELEASE_STORE_FILE)
+        storePassword RELEASE_STORE_PASSWORD
+        keyAlias RELEASE_KEY_ALIAS
+        keyPassword RELEASE_KEY_PASSWORD
     }
 }
 kapt {
     correctErrorTypes = true
 }
 android {
+    namespace "com.bernaferrari.sdkmonitor"
     lintOptions {
-        isAbortOnError = false
+        abortOnError false
+    }
+    defaultConfig {
+        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
+        buildConfigField "boolean", "BETA", "false"
+        manifestPlaceholders = [appIcon: "@drawable/ic_launcher", appRoundIcon: "@null", googleMapsKey: "aNonKey"]
+        vectorDrawables.useSupportLibrary true
+    }
+    flavorDimensions 'brand', 'releaseType'
+    productFlavors {
+        prod {
+            dimension 'releaseType'
+            resValue "string", "myFieldName", "@string/app_name"
+            manifestPlaceholders = [appLabel: "@string/activity_label", deepLink: "@string/deepLink"]
+        }
+    }
+    variantFilter { variant ->
+        setIgnore(false)
+    }
+    dexOptions {
+        javaMaxHeapSize "4g"
+        jumboMode true
+    }
+    dataBinding {
+        enabled true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion "1.3.2"
+    }
+    buildFeatures {
+        aidl false
+        compose true
+        buildConfig true
+        prefab true
+        renderScript true
+        resValues true
+        shaders true
+        dataBinding false
+        viewBinding true
     }
 }
 compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    coreLibraryDesugaringEnabled true
+    sourceCompatibility = '1.8'
+    targetCompatibility = '1.8'
 }
 
-sourceCompatibility = JavaVersion.VERSION_1_8
+sourceCompatibility JavaVersion.VERSION_1_8
 
 dataBinding {
-    isEnabled = true
+    enabled = true
 }
 
-tasks.register<Delete>("clean").configure {
-    delete(rootProject.buildDir)
- }`
+task clean(type: Delete) {
+    delete rootProject.buildDir
+}`
