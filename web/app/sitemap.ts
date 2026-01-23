026e1faa072e425162030628631bed407d85e490
Bernardo Ferrari <bernaferrari2@gmail.com>
2026-01-22 21:48:59 -0300

    Improved SEO

---
+++ web/app/sitemap.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from "next"
+
+import { siteConfig } from "@/config/site"
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: siteConfig.url,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ]
+}
--- web/config/site.ts
+++ web/config/site.ts
@@ -2,7 +2,28 @@ export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
   name: "Gradle to Kotlin Converter",
-  description: "Convert from Groovy to Kotlin DSL (KTS). Open Source.",
+  description:
+    "Free online tool to convert Gradle build scripts from Groovy to Kotlin DSL (KTS). Instantly transform your build.gradle files with accurate syntax conversion for Android and JVM projects.",
+  url: "https://gradlekotlinconverter.app",
+  keywords: [
+    "Gradle",
+    "Kotlin",
+    "Kotlin DSL",
+    "KTS",
+    "Groovy",
+    "build.gradle",
+    "build.gradle.kts",
+    "Android",
+    "converter",
+    "migration",
+    "build script",
+    "Gradle Kotlin DSL",
+  ],
+  author: {
+    name: "Bernardo Ferrari",
+    twitter: "@bernaferrari",
+    github: "bernaferrari",
+  },
   mainNav: [],
   links: {
     twitter: "https://twitter.com/bernaferrari",
