a64c11faf343de842333f1738a020a4cbe85ca3c
Bernardo Ferrari <bernaferrari2@gmail.com>
2026-01-28 15:11:18 -0300

    fix posthog

--- web/instrumentation-client.ts
+++ web/instrumentation-client.ts
@@ -1,10 +1,8 @@
-import posthog from "posthog-js"
+import posthog from "posthog-js";
 
-if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-    api_host:
-      process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
-    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
-    capture_pageview: false, // Disable automatic pageview capture, as we capture manually
-  })
-}
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  defaults: '2025-11-30'
+});
+
+export default posthog;
